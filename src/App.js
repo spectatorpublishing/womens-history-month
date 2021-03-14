f7e7818756ec4752a47ea3099b96241e68eb3667
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2021-03-14 19:49:28 -0400

    Added Nav

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Home } from './containers/home';
-import { Section } from './containers/section';
+import Home from './containers/home';
+import Section from './containers/section';
+import Navbar from './components/navbar';
 
-function App() {
+const App = () => {
   return (
+    <main>
+      <Navbar/>
       <Router>
         <Switch>
             <Route exact path='/' component={Home} />
             <Route exact path='/article' component={Section} />
         </Switch>
       </Router>
+    </main>
   );
 }
 
---
+++ src/components/navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+import { device } from '../device';
+
+const Navbar = () => (
+    <div>
+        <a href="/">
+            Home
+        </a>
+        <a href="/article">
+            Section
+        </a>
+    </div>
+);
+
+export default Navbar;
