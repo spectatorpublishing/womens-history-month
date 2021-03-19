2a1ca484df0bc441250ec065e8acd7abe8223083
Matthew <mdw2166@columbia.edu>
2021-03-19 13:39:31 -0400

    Added branch and component file.

---
+++ src/components/credits.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const Credits = () => (
+    <Body>
+        <Column></Column>
+        <Column></Column>
+    </Body>
+);
+
+export default Credits;
