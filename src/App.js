5a3f15dd9a7578f21432ad48f6143c59d0611951
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2021-03-26 20:14:16 -0400

    fixed routed

--- src/App.js
+++ src/App.js
@@ -11,33 +11,33 @@ const App = () => {
       <Navbar/>
       <Router>
         <Switch>
-            <Route path='/' component={Home} />
+            <Route exact path='/' component={Home} />
             <Route
-              path='/news'
+              exact path='/news'
               render={(props) => (
                 <News {...props} articles={news_articles} header='News' next='Arts & Entertainment' nextLink='/a&e'/>
               )}
             />
             <Route
-              path='/a&e'
+              exact path='/a&e'
               render={(props) => (
                 <News {...props} articles={news_articles} header='Arts & Entertainment' next='Spectrum' nextLink='/spectrum'/>
               )}
             />
             <Route
-              path='/spectrum'
+              exact path='/spectrum'
               render={(props) => (
                 <News {...props} articles={news_articles} header='Spectrum' next='Sports' nextLink='/sports'/>
               )}
             />
             <Route
-              path='/sports'
+              exact path='/sports'
               render={(props) => (
                 <News {...props} articles={news_articles} header='Sports' next='Credits' nextLink='/credits'/>
               )}
             />
             <Route
-              path='/credits'
+              exact path='/credits'
               render={(props) => (
                 <News {...props} articles={news_articles} header='Credits' next='Home' nextLink='/'/>
               )}
